014c4f30689a379ca62c05d40f09d455778b6d00
An Cao <kcj.pop.com@gmail.com>
2015-12-31 16:39:03 +0200

    Fix wrong value assertion.

--- test/codewar_kata/count_digit_test.clj
+++ test/codewar_kata/count_digit_test.clj
@@ -6,7 +6,7 @@
   (testing "count digits"
     (is (= (nb-dig 10 1) 4))
     (is (= (nb-dig 25 1) 11))
-    (is (= (nb-dig 5750 0) 4699))
+    (is (= (nb-dig 5750 0) 4700))
     (is (= (nb-dig 11011 2) 9481))
     (is (= (nb-dig 12224 8) 7733))
     (is (= (nb-dig 11549 1) 11905))
